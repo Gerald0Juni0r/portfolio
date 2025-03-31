c7290d58783f2aa1bf8c95741e1cf5a6c92e41e5
Gerald0Juni0r <geraldo.gj63@gmail.com>
2025-03-31 18:26:11 -0300

    Finalizando a tela 'Home' + Construindo a tela 'about me'

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,33 +1,35 @@
 // import setup from "/src/assets/img/setup.png";
+import minhaFoto from "/src/assets/img/minhaFoto.png";
 import "./About.css";
 
 function App() {
-  return (    
-    <div className="sec-sobre" id="sobre">
-        <div>
-            {/* <img className="sec-sobre-setup" src={setup} alt="setup" /> */}
+    return (    
+        <div className="sec-sobre" id="sobre">
+            <div>
+                {/* <img className="sec-sobre-setup" src={setup} alt="setup" /> */}
+            </div>
+            <div className="sec-sobre-text">
+                <div className="sec-sobre-h2">
+                    <h2  className="cor-verde">SOBRE</h2><h2>MIM</h2>
+                </div>
+                <div className="sec-sobre-p">
+                    <p>
+                    Olá, eu sou Geraldo Júnior, desenvolvedor de software full-stack.
+                    
+                    Blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá 
+                    
+                    Busco atuar na área de TI, contribuindo com minhas habilidades e competências na condição de colaborador efetivo ou temporário.
+                    </p>
+                </div>
+                <div className="sec-sobre-formacao">
+                    <h3>Formação</h3>
+                    <p className="cor-verde">Ciência da Computação - <span>UNINASSAU</span></p>
+                    <p className="cor-verde">Programa Com.Dominio Digital 4.0 - <span>UCE</span></p>
+                </div>
+                <img className="sec-sobre-minha-foto" src={minhaFoto} alt="Foto de Geraldo" />
+            </div>
         </div>
-        <div className="sec-sobre-text">
-            <div className="sec-sobre-h2">
-                <h2  className="cor-verde">SOBRE</h2><h2>MIM</h2>
-            </div>
-            <div className="sec-sobre-p">
-                <p>
-                Olá, eu sou Geraldo Júnior, desenvolvedor de software full-stack.
-                
-                Blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá 
-                
-                Busco atuar na área de TI, contribuindo com minhas habilidades e competências na condição de colaborador efetivo ou temporário.
-                </p>
-            </div>
-            <div className="sec-sobre-formacao">
-                <h3>Formação</h3>
-                <p className="cor-verde">Ciência da Computação - <span>UNINASSAU</span></p>
-                <p className="cor-verde">Programa Com.Dominio Digital 4.0 - <span>UCE</span></p>
-            </div>
-        </div>
-    </div>
-  );
+      );
 }
 
 export default App;
