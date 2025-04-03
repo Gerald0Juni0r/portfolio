4441593347784a3b54c9b07cebd48bdf03bcb56c
Gerald0Juni0r <geraldo.gj63@gmail.com>
2025-04-02 23:29:01 -0300

    Finalizando o layout do 'Projects'

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import "./Projects.css";
 
 
-import setup from "/src/assets/img/setup.png";
+import setupProjects from "/src/assets/img/setup-projects.png";
 
 import project01 from "/src/assets/img/Projects/projeto1.png";
 import project02 from "/src/assets/img/Projects/projeto2.png";
@@ -19,7 +19,7 @@ function Projects() {
     return (
         <section className="sec-projects" id="projetos">
             <div className="sec-projects-img esconder">
-                <img className="sec-projects-setup esconder" src={setup} alt="setup" />
+                <img className="sec-projects-setup esconder" src={setupProjects} alt="setup" />
             </div>
             <div className="sec-projects-cont">
                 <h2 className="sec-projects-h2 ubuntu-bold">MEUS <span className="cor-verde">PROJETOS</span></h2>
